0f3500878fc0e05816d308ac1b914626955e183b
Arnold-Joker <111515023+Arnold-Joker@users.noreply.github.com>
2022-11-04 23:24:56 +0800

    Add files via upload

---
+++ java-s.js
@@ -0,0 +1,84 @@
+const yes = document.getElementById('yes')
+const no = document.getElementById('no')
+
+const no1 = document.getElementById('no1')
+const no2 = document.getElementById('no2')
+const no3 = document.getElementById('no3')
+const no4 = document.getElementById('no4')
+const no5 = document.getElementById('no5')
+const no6 = document.getElementById('no6')
+const no7 = document.getElementById('no7')
+const no8 = document.getElementById('no8')
+const no9 = document.getElementById('no9')
+const no10 = document.getElementById('no10')
+const last = document.getElementById('last-no')
+
+
+yes.addEventListener("click", function(){
+				alert('ohh really')
+				alert('so....')
+				alert('tayo na?...')
+				alert('EHEHEHHEHE')
+				alert('PM me ehehe')
+})
+
+
+no.addEventListener("click", function(){
+				no1.style.opacity="1"
+				no.style.opacity="0"
+})
+
+no1.addEventListener("click", function(){
+				no2.style.opacity="1"
+				no1.style.opacity="0"
+})
+
+no2.addEventListener("click", function(){
+				no3.style.opacity="1"
+				no2.style.opacity="0"
+})
+
+no3.addEventListener("click", function(){
+				no4.style.opacity="1"
+				no3.style.opacity="0"
+})
+
+no4.addEventListener("click", function(){
+				no5.style.opacity="1"
+				no4.style.opacity="0"
+})
+
+no5.addEventListener("click", function(){
+				no6.style.opacity="1"
+				no5.style.opacity="0"
+})
+
+no6.addEventListener("click", function(){
+				no7.style.opacity="1"
+				no6.style.opacity="0"
+})
+
+no7.addEventListener("click", function(){
+				no8.style.opacity="1"
+				no7.style.opacity="0"
+})
+
+no8.addEventListener("click", function(){
+				no9.style.opacity="1"
+				no8.style.opacity="0"
+})
+
+no9.addEventListener("click", function(){
+				no10.style.opacity="1"
+				no9.style.opacity="0"
+})
+
+no10.addEventListener("click", function(){
+				last.style.opacity="1"
+				no10.style.opacity="0"
+})
+
+last.addEventListener('click', function(){
+				alert('oh so u actually dont like me HAHAHAHAHA')
+})
+
